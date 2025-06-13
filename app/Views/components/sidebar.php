cac7bfeff79a5f1e222cf4c7516160b29ae970f3
fitrianv <fitrianovitasari09@gmail.com>
2025-06-13 17:39:21 +0700

    menambahkan api data transaksi pembelian

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -29,6 +29,26 @@
       }
       ?>
 
+      <li class="nav-item">
+          <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
+              <i class="bi bi-person"></i>
+              <span>Profile</span>
+          </a>
+      </li><!-- End Profile Nav -->
+
+      <li class="nav-item">
+          <a class="nav-link <?php echo (uri_string() == 'faq') ? "" : "collapsed" ?>" href="faq">
+              <i class="bi bi-question-circle"></i>
+              <span>F.A.Q</span>
+          </a>
+      </li><!-- End FAQ Nav -->
+
+      <li class="nav-item">
+          <a class="nav-link <?php echo (uri_string() == 'contact') ? "" : "collapsed" ?>" href="contact">
+              <i class="bi bi-envelope"></i>
+              <span>Contact</span>
+          </a>
+      </li><!-- End Contact Nav -->
     </ul>
 
   </aside><!-- End Sidebar-->
